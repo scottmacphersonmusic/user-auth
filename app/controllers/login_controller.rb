7a51f046be043049463c90d42f7413e9f5104456
Scott Macpherson <scottmacphersonmusic@gmail.com>
2015-09-09 13:33:09 -0700

    User can log in

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,4 +1,15 @@
 class LoginController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    if user = User.find_by(:email => params[:user][:email]).try(:authenticate, params[:user][:password])
+      session[:current_user_id] = user.id
+      redirect_to root_url
+    end
+  end
+
   def destroy
     @_current_user = session[:current_user_id] = nil
     redirect_to root_path
